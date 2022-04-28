88722b28dd70296f2ef5e058cdf3d36e1064a230
arun <karthikarun172@gmail.com>
2022-04-28 12:28:52 +0530

    GET Data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [dataValues, setDataValues] = useState([])
+
+
+  const getData = async () => {
+    const post = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    setDataValues(post.data)
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>helo</h2>
+      {dataValues.map(d => (
+        <p key={d.id} >{d.title}</p>
+      ))}
     </div>
   );
 }
